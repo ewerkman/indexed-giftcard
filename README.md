5aadaafb9bbfbec3238c7d6f26bcdc3ac181d2bc
Erwin Werkman <werkman.erwin@gmail.com>
2021-04-19 11:34:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# indexed-giftcard
+Plugin that will index the gift card entity in Sitecore XC 9.3 and above.
---
+++ src/SitecoreServices.Commerce.Plugin.Index/ConfigureSitecore.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Sample.Commerce.Plugin.Index.Pipelines.Blocks;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Plugin.Search;
+using Sitecore.Framework.Configuration;
+using Sitecore.Framework.Pipelines.Definitions.Extensions;
+
+namespace Sample.Commerce.Plugin.Index
+{
+    public class ConfigureSitecore : IConfigureSitecore
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.RegisterAllPipelineBlocks(assembly);
+
+            services.Sitecore().Pipelines(config => config
+                .ConfigurePipeline<IIncrementalIndexMinionPipeline>(c =>
+                    c.Add<InitializeGiftcardsIndexingViewBlock>().After<InitializeIndexingViewBlock>())
+                .ConfigurePipeline<IFullIndexMinionPipeline>(c =>
+                    c.Add<InitializeGiftcardsIndexingViewBlock>().After<InitializeIndexingViewBlock>())
+            );
+
+            services.RegisterAllCommands(assembly);
+        }
+    }
+}
